567ef0462b14b83ef852c3a17998229a820f7def
Jay McAliley <jay.mcaliley@gmail.com>
2016-05-25 11:46:26 -0400

    Add support for IN statements, single quotes

--- test/index_test.js
+++ test/index_test.js
@@ -7,8 +7,44 @@ test('Happy Path', function(t){
     "foo = 'bar'"
   );
   t.equals(
+    w2({foo: 1}),
+    "foo = 1"
+  );
+  t.equals(
+    w2({foo: ['bar']}),
+    "foo IN ('bar')"
+  );
+  t.equals(
+    w2({foo: ['bar', 'baz']}),
+    "foo IN ('bar', 'baz')"
+  );
+  t.equals(
     w2({ foo: 'bar', beep: { $lt: 'boop'}}),
     "foo = 'bar' AND beep < 'boop'"
   )
+  t.equals(
+    w2({foo: ['bar', 'baz'], beep: 'boop'}),
+    "foo IN ('bar', 'baz') AND beep = 'boop'"
+  );
+  t.end();
+});
+
+test('With single quotes', function(t){
+  t.equals(
+    w2({foo: "'bar'"}),
+    "foo = '''bar'''"
+  );
+  t.equals(
+    w2({foo: ["bar'", "'baz'"]}),
+    "foo IN ('bar''', '''baz''')"
+  );
+  t.end();
+});
+
+test('With undefined values', function(t){
+  t.equals(
+    w2({foo: undefined}),
+    "foo = 'undefined'"
+  );
   t.end();
 });
